5812c6fb8834df804a2cb616502f44041b99b797
ssoso27 <sohi8290@gmail.com>
2019-06-07 17:05:10 +0900

    first commit

---
+++ src/main/java/kr/ac/jejunu/tlog/TlogApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.tlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TlogApplication.class, args);
+    }
+
+}
